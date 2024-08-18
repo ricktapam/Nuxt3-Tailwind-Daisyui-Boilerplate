d54bb44dd9aa251772161f2f9172650ca7f23e58
ricktapam <patrick.amiri@outlook.com>
2024-08-18 17:44:39 +0200

    Updated, changed from layout to app.vue

---
+++ app.vue
@@ -0,0 +1,30 @@
+<template>
+
+
+    <Navbar></Navbar>
+    <NuxtPage />
+    <Footer></Footer>
+
+</template>
+
+<script setup lang="ts">
+useHead({
+    title: 'Nuxt',
+    meta: [
+        { name: 'description', content: 'A boilerplate Nuxt-project' }
+    ],
+})
+</script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+    transition: all 0.2s;
+}
+
+.page-enter-from,
+.page-leave-to {
+    opacity: 0;
+    filter: blur(1rem);
+}
+</style>
